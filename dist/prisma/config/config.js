8816759cd6cefc50a0471e869b642cda5e37165c
Victor Siqueira <131918226+victorsz08@users.noreply.github.com>
2025-03-11 08:25:57 -0300

    Delete dist directory

--- dist/prisma/config/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jsonSecret = void 0;
-require("dotenv/config");
-exports.jsonSecret = {
-    secret: process.env.SECRET || ""
-};
--- dist/src/middlewares/api-error.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiError = apiError;
-function apiError(error, request, response, next) {
-    const statusCode = error.statusCode ?? 500;
-    const message = error.statusCode ? error.message : "Internal server error";
-    return response.status(statusCode).json({ status: statusCode, error: message }).send();
-}
-;
